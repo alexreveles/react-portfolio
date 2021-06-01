edd80d7c78b4d09d0cc24fbee9c98ef58a4fda23
alex <alexreveles@yahooo.com>
2021-06-01 13:07:30 -0700

    portfolio updated

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,13 +15,19 @@ function About() {
       />
 
       <p>
-       Full Stack Developer from the UCLA coding bootcamp, during my time there I enjoyed working on a team not only building projects but also tackling problem solving challenges. <br />
-       In the six months of learning many new languages in the bootcamp I implemented some of the newest technologies to build many small projects with my collegues and determined problems,<br />
-        testing methods, and best practices.
-       Seeking to leverage broad development experience and hands-on technical expertise in a challenging role as a Front-end Developer.
-
+        Hello and Welcome to my portfolio, my name is Alejandro Reveles I am
+        Full Stack Developer from the UCLA coding bootcamp, during my time there
+        I enjoyed working on a team not only building projects but also tackling
+        problem solving challenges. In the six months of learning many new
+        languages in the bootcamp I implemented some of the newest technologies
+        to build many small projects with my collegues and determined to solve
+        complicated problems, testing methods, and using best practices. I am
+        Seeking to leverage broad development experience and hands-on technical
+        expertise in a challenging role as a Project Manager, playing sports all
+        my life Ive learned to be not only a team player but also become a
+        leader, I truly have a passion to make sure i lead my team into success
+        and accomplish any task and deadlines on time.
       </p>
-      
     </section>
   );
 }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./stylesheet.css";
 import happyTails from "../../assets/cover/happy-tails.png";
+import happyTailsV2 from "../../assets/cover/happy-tailsV2.png";
 import movieHunt from "../../assets/cover/movie-hunt-app.png";
 import runBuddy from "../../assets/cover/run-buddy.png";
 import weatherDashboard from "../../assets/cover/weather-dashboard.png";
@@ -16,15 +17,41 @@ function Portfolio() {
       <div className="row row-cols-1 row-cols-md-2 g-4">
         <div className="col">
           <div className="card-project">
-            <a href="https://happy-happy-tails.herokuapp.com/">
+            <a href="https://happytails-v2.herokuapp.com/" target="_blank">
+              {/* <img src={happyTailsV2} className="card-img-top" alt="happy-tails-project" /> */}
+
+              <img
+                src={require("../../assets/cover/happy-tailsV2.png").default}
+                className="card-img-top"
+                alt="happy-tails-project"
+              />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">Happy Tails</h5>
+              <p className="card-text">
+                CSS | JavaScript | Materialize | React | Node.JS | Express.JS |
+                MongoDB | MongooseODM | GraphQL | Heroku | Apollo Server | JSON
+                Webtoken | React-hooks | Global State React-Router-Dom | Bcrypt
+                | Stripe-js | and more.
+              </p>
+              <a href="https://happytails-v2.herokuapp.com/" target="_blank" role="button">
+                <button type="button" className="btn btn-outline-dark">
+                  Happy Tails Project
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card-project">
+            <a href="https://happy-happy-tails.herokuapp.com/" target="_blank">
               {/* <img src={happyTails} className="card-img-top" alt="happy-tails-project" /> */}
-              
+
               <img
                 src={require("../../assets/cover/happy-tails.png").default}
                 className="card-img-top"
                 alt="happy-tails-project"
               />
-              
             </a>
             <div className="card-body">
               <h5 className="card-title">Happy Tails</h5>
@@ -33,15 +60,17 @@ function Portfolio() {
                 Express.js | Handlebars | MySQL | RESTful API | ORM | Heroku |
                 brypt
               </p>
-              <a  href="https://happy-happy-tails.herokuapp.com/" role="button">
-              <button type="button" className="btn btn-outline-dark">Happy Tails Project</button>
+              <a href="https://happy-happy-tails.herokuapp.com/" target="_blank" role="button">
+                <button type="button" className="btn btn-outline-dark">
+                  Happy Tails Project
+                </button>
               </a>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card-project">
-            <a href="https://alexreveles.github.io/movie-hunt-app/">
+            <a href="https://alexreveles.github.io/movie-hunt-app/" target="_blank">
               {/* <img
                 src={movieHunt}
                 className="card-img-top"
@@ -61,15 +90,20 @@ function Portfolio() {
                 <br />
                 <br />
               </p>
-              <a  href="https://alexreveles.github.io/movie-hunt-app/" role="button">
-              <button type="button" className="btn btn-outline-dark">Movie Hunt Project</button>
+              <a
+                href="https://alexreveles.github.io/movie-hunt-app/" target="_blank"
+                role="button"
+              >
+                <button type="button" className="btn btn-outline-dark">
+                  Movie Hunt Project
+                </button>
               </a>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card-project">
-            <a href="https://alexreveles.github.io/run-buddy/">
+            <a href="https://alexreveles.github.io/run-buddy/" target="_blank">
               {/* <img
                 src={runBuddy}
                 className="card-img-top"
@@ -84,15 +118,19 @@ function Portfolio() {
             <div className="card-body">
               <h5 className="card-title">Run Buddy</h5>
               <p className="card-text">CSS | HTML</p>
-              <a  href="https://alexreveles.github.io/run-buddy/" role="button">
-              <button type="button" className="btn btn-outline-dark">Run Buddy Project</button>
+              <br />
+              <br />
+              <a href="https://alexreveles.github.io/run-buddy/" target="_blank" role="button">
+                <button type="button" className="btn btn-outline-dark">
+                  Run Buddy Project
+                </button>
               </a>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card-project">
-            <a href="https://alexreveles.github.io/weather-dashboard">
+            <a href="https://alexreveles.github.io/weather-dashboard" target="_blank"> 
               {/* <img
                 src={weatherDashboard}
                 className="card-img-top"
@@ -109,15 +147,20 @@ function Portfolio() {
             <div className="card-body">
               <h5 className="card-title">Weather Dash Board</h5>
               <p className="card-text">HTML | CSS | Bootstrap | JavaScript</p>
-              <a  href="https://alexreveles.github.io/weather-dashboard" role="button">
-              <button type="button" className="btn btn-outline-dark">Weather Dashboard Project</button>
+              <a
+                href="https://alexreveles.github.io/weather-dashboard" target="_blank"
+                role="button"
+              >
+                <button type="button" className="btn btn-outline-dark">
+                  Weather Dashboard Project
+                </button>
               </a>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card-project">
-            <a href="https://alexreveles.github.io/workday-scheduler/">
+            <a href="https://alexreveles.github.io/workday-scheduler/" target="_blank">
               {/* <img
                 src={workdaySchedular}
                 className="card-img-top"
@@ -136,8 +179,13 @@ function Portfolio() {
               <p className="card-text">
                 HTML | CSS | Bootstrap | jQuery | JavaScript
               </p>
-              <a  href="https://alexreveles.github.io/workday-scheduler/" role="button">
-              <button type="button" className="btn btn-outline-dark">Workdat Schedular Project</button>
+              <a
+                href="https://alexreveles.github.io/workday-scheduler/" target="_blank"
+                role="button"
+              >
+                <button type="button" className="btn btn-outline-dark">
+                  Workdat Schedular Project
+                </button>
               </a>
             </div>
           </div>
