9aee855ec63dd65e96830325c14ddde3aece6b4c
alex <alexreveles@yahooo.com>
2021-07-22 15:57:38 -0700

    contact updated

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,112 +1,77 @@
-import React, { useState } from "react";
-import { validateEmail } from "../../utils/helpers";
-import Container from "react-bootstrap/Container";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import "./stylesheet.css";
+import React, { useState } from 'react'
+import { validateEmail } from '../../utils/helpers'
+
 
 function ContactForm() {
-  //Hook that'll manage the form data--feature of this Hook is the ability to initialize the values of the state
-  const [errorMessage, setErrorMessage] = useState("");
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const { name, email, message } = formState;
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-  //handleChange should fire whenever a keystroke is types in the name input
-  const handleChange = (e) => {
-    if (e.target.name === "email") {
-      const isValid = validateEmail(e.target.value);
-      // isValid conditional statement
-      if (!isValid) {
-        setErrorMessage("Your email is invalid.");
-      } else {
-        setErrorMessage("");
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage("");
-      }
+    const { name, email, message } = formState;
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+
+            console.log(isValid);
+
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+
+
+        console.log('errorMessage ' + errorMessage);
     }
-  };
 
-  //form to handle submit
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(formState);
-  }
+    
 
-  console.log(formState);
-  return (
-    <div>
-      <Jumbotron fluid className="shadow p-5 mb-5">
-        <Container className="text-center">
-          <div className="p-md-5">
-            <h1>Contact Me</h1>
-          </div>
-        </Container>
-        <Container className="text-center">
-          <form id="contact-form" onSubmit={handleSubmit}>
-            {/* name input */}
-            <Row>
-              <Col>
-                <div>
-                  <label className="name" htmlFor="name">
-                    Name:{" "}
-                  </label>
-                  <input
-                    type="text"
-                    name="name"
-                    defaultValue={name}
-                    onBlur={handleChange}
-                  />
+    return (
+        <section className="contact-cont rounded-border section-bkg">
+            <div className="row">
+                <div className="col-sm">
+                    <h1 className="p-3">Contact Me : </h1>
+                    <ul className="contact-items">
+                        <li className="contact-items">
+                            Phone: <a className="contact-link" href="tel:7146154168">(805)816-1118</a>
+                        </li>
+                        <br/>
+                        <br/>
+                        <li>
+                            Email: <a className="contact-link" href="mailto:alexreveles@yahoo.com" target="_blank">alexreveles@yahoo.com </a>
+                        </li>
+                        <br/>
+                        <br/>
+                        <li>
+                            LinkedIn: <a className="contact-link" href="https://www.linkedin.com/in/alexreveles11/" target="_blank">LinkedIn Profile</a> 
+                        </li>
+                        <br/>
+                        <br/>
+                        <li>
+                            GitHub: <a className="contact-link" href="https://github.com/alexreveles" target="_blank">Alex Reveles</a> 
+                        </li>
+                        <br/>
+                        <br/>
+                    </ul>
+                    
                 </div>
-
-                {/* email input  */}
-                <div>
-                  <label className="email" htmlFor="email">
-                    Email:{" "}
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    defaultValue={email}
-                    onBlur={handleChange}
-                  />
-                </div>
-              </Col>
-              {/* message text area */}
-              <div>
-                <label className="message" htmlFor="message">
-                  Message:{" "}
-                </label>
-                <textarea
-                  name="message"
-                  rows="5"
-                  defaultValue={message}
-                  onBlur={handleChange}
-                />
-                {/* the below is the same as if(errorMessage) */}
-                {errorMessage && (
-                  <div>
-                    <p className="error-text">{errorMessage}</p>
-                  </div>
-                )}
-              </div>
-            </Row>
-            <a className="contact" href="mailto:alexreveles@yahoo.com">
-            <button type="submit">Submit</button>
-            </a>
-          </form>
-        </Container>
-      </Jumbotron>
-    </div>
-  );
+            </div>
+           
+        </section>
+    );
 }
+
 
 export default ContactForm;
