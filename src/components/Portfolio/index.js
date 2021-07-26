20e51d2b9e5ae85e9dd771b981735548fe79f9e4
alex <alexreveles@yahooo.com>
2021-07-26 13:18:10 -0700

    updated portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -27,16 +27,40 @@ function Portfolio() {
               />
             </a>
             <div className="card-body">
-              <h5 className="card-title">Happy Tails</h5>
+              <h5 className="card-title">Happy Tails 2.0</h5>
+              <p>
+                {" "}
+                As a shelter employee / volunteer, I want to track the dogs
+                daily activities and dogs difficulty levels so that all the dogs
+                are taken care of and paired with a compatible caretaker that
+                meets the required experience. As a shelter administrator I want
+                to track daily activity, add dogs, as well as be able to
+                de-activate / re-activate users and dogs.
+              </p>
               <p className="card-text">
                 CSS | JavaScript | Materialize | React | Node.JS | Express.JS |
                 MongoDB | MongooseODM | GraphQL | Heroku | Apollo Server | JSON
                 Webtoken | React-hooks | Global State React-Router-Dom | Bcrypt
                 | Stripe-js | and more.
               </p>
-              <a href="https://happytails-v2.herokuapp.com/" target="_blank" role="button">
+              <a
+                href="https://happytails-v2.herokuapp.com/"
+                target="_blank"
+                role="button"
+              >
                 <button type="button" className="btn btn-outline-dark">
-                  Happy Tails Project
+                  Deployed Link
+                </button>
+              </a>
+              <br />
+              <br />
+              <a
+                href="https://github.com/alexreveles/happytails-v2"
+                target="_blank"
+                role="button"
+              >
+                <button type="button" className="btn btn-outline-dark">
+                  Github Link
                 </button>
               </a>
             </div>
@@ -55,22 +79,48 @@ function Portfolio() {
             </a>
             <div className="card-body">
               <h5 className="card-title">Happy Tails</h5>
+              <p>
+                A a shelter employee/volunteer, I WANT to track the dogs daily
+                activities and dogs difficulty levels so that all the dogs are
+                taken care of and paired with a compatible caretaker that meets
+                the required experience.
+              </p>
               <p className="card-text">
                 HTML | CSS | Materialize | JavaScript | Sequelize | Node.js |
                 Express.js | Handlebars | MySQL | RESTful API | ORM | Heroku |
                 brypt
               </p>
-              <a href="https://happy-happy-tails.herokuapp.com/" target="_blank" role="button">
+              <a
+                href="https://happy-happy-tails.herokuapp.com/"
+                target="_blank"
+                role="button"
+              >
                 <button type="button" className="btn btn-outline-dark">
-                  Happy Tails Project
+                  Deployed Link
                 </button>
               </a>
+              <br />
+              <br />
+              <br />
+              <a
+                href="https://github.com/alexreveles/happy-tails"
+                target="_blank"
+                role="button"
+              >
+                <button type="button" className="btn btn-outline-dark">
+                  Github Link
+                </button>
+              </a>
+              <br />
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card-project">
-            <a href="https://alexreveles.github.io/movie-hunt-app/" target="_blank">
+            <a
+              href="https://alexreveles.github.io/movie-hunt-app/"
+              target="_blank"
+            >
               {/* <img
                 src={movieHunt}
                 className="card-img-top"
@@ -84,18 +134,37 @@ function Portfolio() {
             </a>
             <div className="card-body">
               <h5 className="card-title">Movie Hunt</h5>
+              <p>
+                Movie hunt is an application where users can input a search of a
+                movie title and receive relevant information. Some of the
+                information returned from the search includes the movie plot,
+                year of release, rating, genre, and movie poster. This will
+                allow users to find the perfect movie to watch on there free
+                time.
+              </p>
               <p className="card-text">
                 HTML | CSS | Materialize | JavaScript | JQuery | Modal
                 <br />
                 <br />
-                <br />
               </p>
               <a
-                href="https://alexreveles.github.io/movie-hunt-app/" target="_blank"
+                href="https://alexreveles.github.io/movie-hunt-app/"
+                target="_blank"
                 role="button"
               >
                 <button type="button" className="btn btn-outline-dark">
-                  Movie Hunt Project
+                  Deployed Link
+                </button>
+              </a>
+              <br />
+              <br />
+              <a
+                href="https://github.com/alexreveles/movie-hunt-app"
+                target="_blank"
+                role="button"
+              >
+                <button type="button" className="btn btn-outline-dark">
+                  Github Link
                 </button>
               </a>
             </div>
@@ -117,12 +186,35 @@ function Portfolio() {
             </a>
             <div className="card-body">
               <h5 className="card-title">Run Buddy</h5>
+              <p>
+                A website that offers fitness training services and more, it is
+                a friendly user website where you are able to select your
+                trainer and see who they are and there specialty in fitness.
+                Allowing users to select the correct run buddy for there level
+                of experience. It is the best application to find a trainer and
+                a friend in fitness.
+              </p>
               <p className="card-text">CSS | HTML</p>
               <br />
               <br />
-              <a href="https://alexreveles.github.io/run-buddy/" target="_blank" role="button">
+              <a
+                href="https://alexreveles.github.io/run-buddy/"
+                target="_blank"
+                role="button"
+              >
                 <button type="button" className="btn btn-outline-dark">
-                  Run Buddy Project
+                  Deployed Link
+                </button>
+              </a>
+              <br />
+              <br />
+              <a
+                href="https://github.com/alexreveles/run-buddy"
+                target="_blank"
+                role="button"
+              >
+                <button type="button" className="btn btn-outline-dark">
+                  Github Link
                 </button>
               </a>
             </div>
@@ -130,7 +222,10 @@ function Portfolio() {
         </div>
         <div className="col">
           <div className="card-project">
-            <a href="https://alexreveles.github.io/weather-dashboard" target="_blank"> 
+            <a
+              href="https://alexreveles.github.io/weather-dashboard"
+              target="_blank"
+            >
               {/* <img
                 src={weatherDashboard}
                 className="card-img-top"
@@ -146,13 +241,34 @@ function Portfolio() {
             </a>
             <div className="card-body">
               <h5 className="card-title">Weather Dash Board</h5>
+              <p>
+                A weather application that allows you to select your city and
+                retrieve the weather data accurate to the weather bradcast, it
+                is an application to give you a heads up on the current weather
+                and the following weather for the next couple days describing
+                the climate and giving you an exact detail of the days
+                broadcast.
+              </p>
+
               <p className="card-text">HTML | CSS | Bootstrap | JavaScript</p>
               <a
-                href="https://alexreveles.github.io/weather-dashboard" target="_blank"
+                href="https://alexreveles.github.io/weather-dashboard"
+                target="_blank"
                 role="button"
               >
                 <button type="button" className="btn btn-outline-dark">
-                  Weather Dashboard Project
+                  Deployed Link
+                </button>
+              </a>
+              <br />
+              <br />
+              <a
+                href="https://github.com/alexreveles/weather-dashboard"
+                target="_blank"
+                role="button"
+              >
+                <button type="button" className="btn btn-outline-dark">
+                  Github Link
                 </button>
               </a>
             </div>
@@ -160,7 +276,10 @@ function Portfolio() {
         </div>
         <div className="col">
           <div className="card-project">
-            <a href="https://alexreveles.github.io/workday-scheduler/" target="_blank">
+            <a
+              href="https://alexreveles.github.io/workday-scheduler/"
+              target="_blank"
+            >
               {/* <img
                 src={workdaySchedular}
                 className="card-img-top"
@@ -176,15 +295,35 @@ function Portfolio() {
             </a>
             <div className="card-body">
               <h5 className="card-title">Work Day</h5>
+              <p>
+                A calendar application that allows users to save events for each
+                working hour from 9am to 5pm. This application allows you to see
+                what tasks are needed to complete for the hour, as the hour is
+                passed it will highligh in red, meaning that task for the hour
+                is due. If it is highlighted in green then that task is
+                approaching as it is needed to be completed.
+              </p>
               <p className="card-text">
                 HTML | CSS | Bootstrap | jQuery | JavaScript
               </p>
               <a
-                href="https://alexreveles.github.io/workday-scheduler/" target="_blank"
+                href="https://alexreveles.github.io/workday-scheduler/"
+                target="_blank"
                 role="button"
               >
                 <button type="button" className="btn btn-outline-dark">
-                  Workdat Schedular Project
+                  Deployed Link
+                </button>
+              </a>
+              <br />
+              <br />
+              <a
+                href="https://github.com/alexreveles/workday-scheduler"
+                target="_blank"
+                role="button"
+              >
+                <button type="button" className="btn btn-outline-dark">
+                  Github Link
                 </button>
               </a>
             </div>
